feda980f5e03549dd0bbca27bc9acdd8db16a01e
jaydenpiao <jaydenpiao@gmail.com>
2023-11-19 21:30:53 -0800

    solved majority element

---
+++ solutions/169.majority-element.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=169 lang=python
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[len(nums) / 2]
+        
+        
+# @lc code=end
+
