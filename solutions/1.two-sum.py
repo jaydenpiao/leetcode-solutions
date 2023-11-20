97dbae3472edaf421bbf6587660ff0e3f2d89eb1
jaydenpiao <jaydenpiao@gmail.com>
2023-11-19 19:14:29 -0800

    solved two sum

--- solutions/1.two-sum.py
+++ solutions/1.two-sum.py
@@ -12,6 +12,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        num_map = {}
+        for i in range(len(nums)):
+            if target - nums[i] in num_map:
+                return [num_map[target - nums[i]], i]
+            num_map[nums[i]] = i
+        return []
         
 # @lc code=end
 
