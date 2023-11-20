92b28503d24cc38b92625a55c1751f01aeff49ee
jaydenpiao <jaydenpiao@gmail.com>
2023-11-20 00:19:26 -0800

    solved find idx of first occurrence in string

---
+++ solutions/strings/28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=28 lang=python
+#
+# [28] Find the Index of the First Occurrence in a String
+#
+
+# @lc code=start
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            return haystack.index(needle)
+        return -1
+        
+# @lc code=end
+
