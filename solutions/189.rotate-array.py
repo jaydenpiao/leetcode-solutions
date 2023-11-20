c7bd0633ba66633bd40a6da27a60210ba56b4201
jaydenpiao <jaydenpiao@gmail.com>
2023-11-19 22:51:55 -0800

    solved rotate array

---
+++ solutions/189.rotate-array.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=189 lang=python
+#
+# [189] Rotate Array
+#
+
+# @lc code=start
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start, end = start + 1, end - 1
+        k = k % len(nums)
+        reverse(0, len(nums) - 1)
+        reverse(0, k-1)
+        reverse(k, len(nums) - 1)
+
+# @lc code=end
+
