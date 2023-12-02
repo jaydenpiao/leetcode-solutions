4adee5fbebea7ed2fbd7c14c55764a83be0d6225
jaydenpiao <jaydenpiao@gmail.com>
2023-12-02 01:16:43 -0800

    solved remove element

--- solutions_java/arrays/27.remove-element.java
+++ solutions_java/arrays/27.remove-element.java
@@ -7,8 +7,22 @@
 // @lc code=start
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            if (nums[start] == val) {
+                while (start<end && nums[end] == val) {
+                    end--;
+                }
+                nums[start] = nums[end];
+                end--;
+            }
+            start++;
+        }
+        return end+1;
     }
+
 }
 // @lc code=end
 
