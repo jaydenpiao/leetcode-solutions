9fb5f32bb69af83ac90d48c9b58863ce05ea9795
jaydenpiao <jaydenpiao@gmail.com>
2023-12-02 00:37:46 -0800

    init java

---
+++ solutions_java/arrays/88.merge-sorted-array.java
@@ -0,0 +1,15 @@
+package solutions_java.arrays;
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+    }
+}
+// @lc code=end
+
---
+++ solutions_python/linkedlists/21.merge-two-sorted-lists.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=21 lang=python
+#
+# [21] Merge Two Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        it1 = list1
+        it2 = list2
+
+        while it1 or it2:
+            
+        
+# @lc code=end
+
