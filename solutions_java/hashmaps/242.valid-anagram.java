701f4c88b9a87aa12a895e0846e40bdc42cb9ea9
jaydenpiao <jaydenpiao@gmail.com>
2023-12-07 03:25:29 -0800

    solved valid anagram

---
+++ solutions_java/hashmaps/242.valid-anagram.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i=0; i<s.length(); i++) {
+            char sChar = s.charAt(i);
+            if (!map.containsKey(sChar)) {
+                map.put(sChar, 1);
+            } else {
+                map.put(sChar, map.get(sChar)+1);
+            }
+        }
+        for (int i=0; i<t.length(); i++) {
+            char tChar = t.charAt(i);
+            if (map.containsKey(tChar) && map.get(tChar) > 0) {
+                map.put(tChar, map.get(tChar)-1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
