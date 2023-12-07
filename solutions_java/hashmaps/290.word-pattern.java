5569e6590a38b37782ba0170062956a501ccab9f
jaydenpiao <jaydenpiao@gmail.com>
2023-12-07 03:15:51 -0800

    solved word pattern

---
+++ solutions_java/hashmaps/290.word-pattern.java
@@ -0,0 +1,15 @@
+package solutions_java.hashmaps;
+/*
+ * @lc app=leetcode id=290 lang=java
+ *
+ * [290] Word Pattern
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        
+    }
+}
+// @lc code=end
+
